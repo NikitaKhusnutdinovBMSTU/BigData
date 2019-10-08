728f1b91a94b0506bd32cab2468a5142e95917a4
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-08 17:41:56 +0300

    Scripted auto-commit on change (2019-10-08 17:41:56) by gitwatch.sh

--- src/main/java/FlightTimeArrival.java
+++ src/main/java/FlightTimeArrival.java
@@ -1,5 +1,9 @@
 public class FlightTimeArrival {
-    public static void main(Sting[] args) throw Exception {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1){
+            System.err.println("Analysing time arrival of planes\nUsage: <output path>");
+            System.exit(-1);
+        }
 
     }
 }

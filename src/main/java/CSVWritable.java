9f0c4e1ea7a9763c5b4dc6385afa141a256240e0
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-08 18:19:28 +0300

    Scripted auto-commit on change (2019-10-08 18:19:28) by gitwatch.sh

--- src/main/java/CSVWritable.java
+++ src/main/java/CSVWritable.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import org.apache.hadoop.io.Writable;
 
-public class CSVWritable {
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class CSVWritable extends Writable {
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }

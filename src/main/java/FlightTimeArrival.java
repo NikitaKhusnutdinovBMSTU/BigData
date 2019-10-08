3990810e69950b1456b33a5d8db7a1a7ac2ad2f5
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-08 17:47:31 +0300

    Scripted auto-commit on change (2019-10-08 17:47:31) by gitwatch.sh

--- src/main/java/FlightTimeArrival.java
+++ src/main/java/FlightTimeArrival.java
@@ -1,9 +1,16 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class FlightTimeArrival {
     public static void main(String[] args) throws Exception {
         if (args.length != 1){
             System.err.println("Analysing time arrival of planes\nUsage: <output path>");
             System.exit(-1);
         }
-
+        Job job = Job.getInstance();
+        
     }
 }

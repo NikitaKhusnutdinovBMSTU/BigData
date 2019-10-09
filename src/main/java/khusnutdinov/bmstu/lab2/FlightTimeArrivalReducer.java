e67e04f0ca9dc80c27cc01fb47fa4140b92bdf8e
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-09 15:28:06 +0300

    Scripted auto-commit on change (2019-10-09 15:28:06) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/FlightTimeArrivalReducer.java
+++ src/main/java/khusnutdinov/bmstu/lab2/FlightTimeArrivalReducer.java
@@ -15,5 +15,34 @@ public class FlightTimeArrivalReducer extends Reducer<SharedKey, Text, Text, Tex
     protected void reduce(SharedKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
         Iterator iterator = values.iterator();
+        Double min = Double.MAX_VALUE, max = Double.MIN_VALUE, currentDelay, sum = 0.0;
+        int counter = 0;
+        String airportName = iterator.next().toString();
+        while(iterator.hasNext()){
+            String token = iterator.next().toString();
+            if (token.length() == 0){
+                continue;
+            }
+
+            currentDelay = new Double(token);
+            if (currentDelay == 0.0){
+                continue;
+            }
+            counter++;
+            if(counter == 0 || min > currentDelay){
+                min = currentDelay;
+            }
+            if(counter == 0 || max < currentDelay){
+                max = currentDelay;
+            }
+            sum += currentDelay;
+        }
+        if(counter > 0){
+            String infoString = "".concat("[Min: ".concat(min.toString().concat(", ")));
+            infoString = infoString.concat("Max: ").concat(max.toString()).concat(", ");
+            infoString = infoString.concat("Avg: ").concat(new Double(sum/counter).toString()).concat("];");
+            context.write(new Text(airportName), new Text(infoString));
+        }
+
     }
 }

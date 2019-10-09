1a266e836ad848971a4c693f8be5f7d0aa19b3c2
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-09 15:13:26 +0300

    Scripted auto-commit on change (2019-10-09 15:13:26) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/FlightTimeArrivalReducer.java
+++ src/main/java/khusnutdinov/bmstu/lab2/FlightTimeArrivalReducer.java
@@ -1,4 +1,19 @@
 package khusnutdinov.bmstu.lab2;
 
-public class FlightTimeArrivalReducer {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class FlightTimeArrivalReducer extends Reducer<SharedKey, Text, Text, Text> {
+    /*
+        Функция reduce должна извлекать имя аэропорта а затем расчитывать
+        среднее/минимальное/максимальное время задержки
+                                                                           */
+    @Override
+    protected void reduce(SharedKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        
+        Iterator iterator = values.iterator();
+    }
 }

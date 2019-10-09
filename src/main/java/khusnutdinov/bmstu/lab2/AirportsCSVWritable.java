3e9ae9c2015cd39460137823bfe268c55096fc8f
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-09 14:19:20 +0300

    Scripted auto-commit on change (2019-10-09 14:19:20) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVWritable.java
+++ src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVWritable.java
@@ -1,33 +1,13 @@
 package khusnutdinov.bmstu.lab2;
 
 import javafx.util.Pair;
-import org.apache.hadoop.io.Writable;
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
 
 public class AirportsCSVWritable {
+    private Pair<String, String> airCSVPair;
 
-    private final static int END_OF_COLUMNS = 17;
-    private final static int DEST_AIRPORT_ID_INDEX = 0;
-    private final static int FLIGHT_DELAY_INDEX = 0;
-
-    private Pair<String, String> csvDataPair;
-
-    public AirportsCSVWritable(String dataIn){
-       String[] table = dataIn.split(" ");
-       this.csvDataPair = new Pair<>(table[DEST_AIRPORT_ID_INDEX], table[FLIGHT_DELAY_INDEX]);
+    public AirportsCSVWritable(String airportCSV){
+        String[] twoFoldColumn = airportCSV.split(" ");
+        this.airCSVPair = new Pair<>(twoFoldColumn[0], twoFoldColumn[1]);
     }
 
-    public Pair<String, String> getAirportsCSVPair(){
-        return csvDataPair;
-    }
-
-    public String getDestAirportID(){
-        return csvDataPair.getKey();
-    }
-
-    public String getFlightDelay(){
-        return csvDataPair.getValue();
-    }
 }
---
+++ src/main/java/khusnutdinov/bmstu/lab2/FlightsCSVWritable.java
@@ -0,0 +1,32 @@
+package khusnutdinov.bmstu.lab2;
+
+import javafx.util.Pair;
+import org.apache.hadoop.io.Writable;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class FlightsCSVWritable {
+
+    private final static int DEST_AIRPORT_ID_INDEX = 14;
+    private final static int FLIGHT_DELAY_INDEX = 17;
+
+    private Pair<String, String> csvFlightPair;
+
+    public FlightsCSVWritable(String flightCSV){
+       String[] table = flightCSV.split(" ");
+       this.csvFlightPair = new Pair<>(table[DEST_AIRPORT_ID_INDEX], table[FLIGHT_DELAY_INDEX]);
+    }
+
+    public Pair<String, String> getAirportsCSVPair(){
+        return csvFlightPair;
+    }
+
+    public String getDestAirportID(){
+        return csvFlightPair.getKey();
+    }
+
+    public String getFlightDelay(){
+        return csvFlightPair.getValue();
+    }
+}

7bb92bf02eca4329666b25c8fd0924e9b638b367
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-09 12:41:18 +0300

    Scripted auto-commit on change (2019-10-09 12:41:18) by gitwatch.sh

--- src/main/java/AirportCSVWritable.java
+++ src/main/java/AirportCSVWritable.java
@@ -1,8 +1,9 @@
+import javafx.util.Pair;
 import org.apache.hadoop.io.Writable;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
 public class AirportCSVWritable {
-    private pair
+    private Pair<String, String> airPair;
 }

2df2dd208fafccc2d759a2037ae97f2c4d7a351e
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-09 14:28:27 +0300

    Scripted auto-commit on change (2019-10-09 14:28:27) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVMapper.java
+++ src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVMapper.java
@@ -5,12 +5,14 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.Mapper.Context
 
 import java.io.IOException;
 
 public class AirportsCSVMapper extends Mapper<LongWritable, Text, SharedKey, Text>{
 
-    public void map(LongWritable )
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+
+    }
 
 }

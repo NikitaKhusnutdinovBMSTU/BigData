b74158892557da7b7f77e2bb9635285b058c257f
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-09 14:55:39 +0300

    Scripted auto-commit on change (2019-10-09 14:55:39) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVMapper.java
+++ src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVMapper.java
@@ -4,7 +4,6 @@ import javafx.util.Pair;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Mapper.Context
 
 import java.io.IOException;
 
--- src/main/java/khusnutdinov/bmstu/lab2/FlightsCSVMapper.java
+++ src/main/java/khusnutdinov/bmstu/lab2/FlightsCSVMapper.java
@@ -1,4 +1,17 @@
 package khusnutdinov.bmstu.lab2;
 
-public class FlightsCSVMapper extend Mapper<LongWritable,> {
+import javafx.util.Pair;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+
+public class FlightsCSVMapper extends Mapper<LongWritable, Text, SharedKey, Text> {
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        FlightsCSVWritable flightsCSVWritable = new FlightsCSVWritable(value.toString());
+        
+    }
 }

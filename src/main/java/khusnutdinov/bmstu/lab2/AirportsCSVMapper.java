61ca56d3221fd164a1de4d9eeb41426f7e87133b
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-09 14:23:19 +0300

    Scripted auto-commit on change (2019-10-09 14:23:19) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVMapper.java
+++ src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVMapper.java
@@ -1,11 +1,33 @@
 package khusnutdinov.bmstu.lab2;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
 
-public class AirportsCSVMapper {
+import java.io.IOException;
+
+public class AirportsCSVMapper extends Mapper<LongWritable, Text, SharedKey, Text>{
 
 
     public AirportsCSVMapper() {
+
+    }
+
+    @Override
+    public void map(LongWritable longWritable, Text text, OutputCollector<SharedKey, Text> outputCollector, Reporter reporter) throws IOException {
+        
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
+
+    @Override
+    public void configure(JobConf jobConf) {
 
     }
 }

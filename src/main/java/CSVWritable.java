35105285511d88ddb9e0adf824d8562422f08896
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-09 12:38:17 +0300

    Scripted auto-commit on change (2019-10-09 12:38:17) by gitwatch.sh

--- src/main/java/CSVWritable.java
+++ src/main/java/CSVWritable.java
@@ -4,15 +4,6 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class CSVWritable implements Writable {
+public class CSVWritable {
 
-    @Override
-    public void write(DataOutput dataOutput) throws IOException {
-
-    }
-
-    @Override
-    public void readFields(DataInput dataInput) throws IOException {
-
-    }
 }

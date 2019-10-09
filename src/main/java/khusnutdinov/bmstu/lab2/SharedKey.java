46a31fd0e279406e2c7fd919cd00cb874b0f64d3
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-09 13:19:18 +0300

    Scripted auto-commit on change (2019-10-09 13:19:18) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/SharedKey.java
+++ src/main/java/khusnutdinov/bmstu/lab2/SharedKey.java
@@ -1,5 +1,5 @@
 package khusnutdinov.bmstu.lab2;
 
-/*ключ, который мы будем использовать для reduce side join (1 - это аэропорт, 0 - это перелет) */
+/*ключ, который мы будем использовать для reduce side join (0 - это аэропорт, 1 - это перелет) */
 public class SharedKey {
 }

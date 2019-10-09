799744cf3de70b9194d6f5e50d0b1b9d1da03f5f
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-09 12:58:41 +0300

    Scripted auto-commit on change (2019-10-09 12:58:41) by gitwatch.sh

--- src/main/java/FlightTimeArrival.java
+++ src/main/java/FlightTimeArrival.java
@@ -2,10 +2,12 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class FlightTimeArrival {
+    private String 
     public static void main(String[] args) throws Exception {
         if (args.length != 1){
             System.err.println("Analysing time arrival of planes\nUsage: <output path>");
@@ -14,7 +16,7 @@ public class FlightTimeArrival {
         Job job = Job.getInstance();
         job.setJarByClass(FlightTimeArrival.class);
         job.setJobName("FlightTimeArrival");
-        FileInputFormat.addInputPath(job, new Path("~/Desktop/hadoop/labs/2/664600583_T_ONTIME_sample.csv"));
+        MultipleInputs.addInputPath(job, new Path("~/Desktop/hadoop/labs/2/664600583_T_ONTIME_sample.csv"), TextInputFormat.class, null);
 
     }
 }

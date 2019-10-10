8b15ace7d8299ed86ff75043e049238fb0470b3a
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-10 19:38:51 +0300

    Scripted auto-commit on change (2019-10-10 19:38:51) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/FlightArrivalComparator.java
+++ src/main/java/khusnutdinov/bmstu/lab2/FlightArrivalComparator.java
@@ -1,4 +1,19 @@
 package khusnutdinov.bmstu.lab2;
 
-public class FlightArrivalComparator {
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class FlightArrivalComparator extends WritableComparator {
+
+    // чекаем только код аэропорта
+    public FlightArrivalComparator(){
+        super(SharedKey.class, true);
+    }
+
+    public int compare(WritableComparable a, WritableComparable b){
+        SharedKey sharedKey1 = (SharedKey) a;
+        SharedKey sharedKey2 = (SharedKey) b;
+
+        return sharedKey1.compareTo(sharedKey2);
+    }
 }

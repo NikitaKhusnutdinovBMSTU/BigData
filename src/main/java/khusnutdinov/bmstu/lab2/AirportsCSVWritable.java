36d65453b86cc44c15d1a2913da224b07f251e9e
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-11 23:38:01 +0300

    Scripted auto-commit on change (2019-10-11 23:38:01) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVWritable.java
+++ src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVWritable.java
@@ -5,14 +5,14 @@ import javafx.util.Pair;
 public class AirportsCSVWritable {
     private Pair<String, String> airCSVPair;
 
-    public AirportsCSVWritable(String airportCSV){
+    public AirportsCSVWritable(String airportCSV) {
         if(!airportCSV.contains("Code")) {
             int divide = airportCSV.indexOf(",");
             this.airCSVPair = new Pair<>(airportCSV.substring(0, divide), airportCSV.substring(divide + 2, airportCSV.length() - 1));
         }
     }
 
-    public Pair<String, String> getAirportsPair(){
+    public Pair<String, String> getAirportsPair() {
         return airCSVPair;
     }
 

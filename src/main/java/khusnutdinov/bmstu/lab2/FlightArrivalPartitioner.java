8ac124e6939fbab27a05335a69a07a25d2c57e41
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-11 21:40:24 +0300

    Scripted auto-commit on change (2019-10-11 21:40:24) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/FlightArrivalPartitioner.java
+++ src/main/java/khusnutdinov/bmstu/lab2/FlightArrivalPartitioner.java
@@ -1,7 +1,15 @@
 package khusnutdinov.bmstu.lab2;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class FlightArrivalPartitioner extends Partitioner {
-    
+    public FlightArrivalPartitioner(){
+        //pass
+    }
+
+    public int getPartition(SharedKey key, Text value, int numReduceTasks){
+        int b = new Integer(key.getID());
+        return b % numReduceTasks;
+    }
 }

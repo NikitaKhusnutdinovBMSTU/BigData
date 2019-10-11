1b8ed01d8006fe16ac307cdfb9b89358ee67b741
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-12 00:19:32 +0300

    Scripted auto-commit on change (2019-10-12 00:19:32) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/FlightsCSVMapper.java
+++ src/main/java/khusnutdinov/bmstu/lab2/FlightsCSVMapper.java
@@ -13,6 +13,8 @@ public class FlightsCSVMapper extends Mapper<LongWritable, Text, SharedKey, Text
     public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         FlightsCSVWritable flightsCSVWritable = new FlightsCSVWritable(value.toString());
         Pair<String, String> flightsPair = flightsCSVWritable.getFlightsCSVPair();
-        context.write(new SharedKey(flightsPair.getKey(), 1), new Text(flightsPair.getValue()));
+        if(flightsPair != null){
+            context.write(new SharedKey(flightsPair.getKey(), 1), new Text(flightsPair.getValue()));
+        }
     }
 }

a81c3f07444e246c14854e622637bfa3fbbbb1cb
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-12 00:43:48 +0300

    Scripted auto-commit on change (2019-10-12 00:43:48) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/FlightArrivalPartitioner.java
+++ src/main/java/khusnutdinov/bmstu/lab2/FlightArrivalPartitioner.java
@@ -9,7 +9,7 @@ public class FlightArrivalPartitioner extends Partitioner<SharedKey, Text> {
     }
 
     public int getPartition(SharedKey key, Text value, int numReduceTasks){
-        int b = Integer.valueOf(key.getID());
+        int b = Integer.valueOf(key.getID().split("\\s+"));
         return b % numReduceTasks;
     }
 }

afc192229d607d83a0b768e7874840721ce37e02
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-12 00:32:18 +0300

    Scripted auto-commit on change (2019-10-12 00:32:18) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/FlightTimeArrivalReducer.java
+++ src/main/java/khusnutdinov/bmstu/lab2/FlightTimeArrivalReducer.java
@@ -1,5 +1,7 @@
 package khusnutdinov.bmstu.lab2;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
@@ -11,12 +13,16 @@ public class FlightTimeArrivalReducer extends Reducer<SharedKey, Text, Text, Tex
         Функция reduce должна извлекать имя аэропорта а затем расчитывать
         среднее/минимальное/максимальное время задержки
                                                                            */
+
+    public static final Log log = LogFactory.getLog(FlightTimeArrivalReducer.class);
+
     @Override
     protected void reduce(SharedKey key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
         Iterator iterator = values.iterator();
         Double min = Double.MAX_VALUE, max = Double.MIN_VALUE, currentDelay, sum = 0.0;
         int counter = 0;
+        log.info(iterator.next().toString());
         String airportName = iterator.next().toString();
         while(iterator.hasNext()){
 

2e14ff7b61f1acc33b52362dabee62db51dd0179
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-12 11:37:18 +0300

    Scripted auto-commit on change (2019-10-12 11:37:18) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVWritable.java
+++ src/main/java/khusnutdinov/bmstu/lab2/AirportsCSVWritable.java
@@ -1,8 +1,13 @@
 package khusnutdinov.bmstu.lab2;
 
 import javafx.util.Pair;
+import org.apache.hadoop.io.Writable;
 
-public class AirportsCSVWritable {
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportsCSVWritable implements Writable {
     private Pair<String, String> airCSVPair;
 
     public AirportsCSVWritable(String airportCSV) {
@@ -21,4 +26,23 @@ public class AirportsCSVWritable {
         return airCSVPair;
     }
 
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeChars(airCSVPair.getKey());
+        dataOutput.writeChars(airCSVPair.getValue());
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        String airportCSV = dataInput.readLine();
+        int divide;
+        String id, nameAirport;
+        if(!airportCSV.contains("Description")) {
+            divide = airportCSV.indexOf(",");
+            id = airportCSV.substring(2, divide - 2).replace("\"", "");
+            nameAirport = airportCSV.substring(divide + 2, airportCSV.length() - 1).replace("\"", "");
+            System.out.println( "\n***********\nName of airport is : " + nameAirport + "************\n");
+            airCSVPair = new Pair<>(id, nameAirport);
+        }
+    }
 }

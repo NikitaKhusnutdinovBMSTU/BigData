6f4e1f85f2eee8b9af19e968487f54d9e325e122
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-12 11:38:17 +0300

    Scripted auto-commit on change (2019-10-12 11:38:17) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/FlightsCSVWritable.java
+++ src/main/java/khusnutdinov/bmstu/lab2/FlightsCSVWritable.java
@@ -6,7 +6,7 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class FlightsCSVWritable {
+public class FlightsCSVWritable implements Writable{
 
     private final static int DEST_AIRPORT_ID_INDEX = 14;
     private final static int FLIGHT_DELAY_INDEX = 17;
@@ -37,5 +37,27 @@ public class FlightsCSVWritable {
 
     public String getFlightDelay(){
         return csvFlightPair.getValue();
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeChars(csvFlightPair.getKey());
+        dataOutput.writeChars(csvFlightPair.getValue());
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        String flightCSV = dataInput.readLine();
+        String id, delay;
+        //избавляемся от первой колонки
+        if(flightCSV.length() > 1 && !flightCSV.contains("YEAR")) {
+            String[] table = flightCSV.split(",");
+            if(table.length < 18){
+                return;
+            }
+            id = table[DEST_AIRPORT_ID_INDEX];
+            delay = table[FLIGHT_DELAY_INDEX];
+            this.csvFlightPair = new Pair<>(id, delay);
+        }
     }
 }

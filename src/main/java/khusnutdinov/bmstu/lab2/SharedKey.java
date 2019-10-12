4b4c66f47ca5985f33a38b49d7c5971e143ba96a
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-12 10:34:53 +0300

    Scripted auto-commit on change (2019-10-12 10:34:53) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/SharedKey.java
+++ src/main/java/khusnutdinov/bmstu/lab2/SharedKey.java
@@ -56,6 +56,6 @@ public class SharedKey implements WritableComparable<SharedKey> {
         int otherFlag = other.getFlag();
         /* если они равны, то тогда сравниваем по флагу,
             иначе отправляем сравнение строк            */
-        return stringCompare == 0 ? flag - otherFlag : stringCompare;
+        return stringCompare == 0 ? stringCompare : flag - otherFlag;
     }
 }

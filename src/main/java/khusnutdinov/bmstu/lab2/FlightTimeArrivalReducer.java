b07b00563fcd34d9f5bd9dc6d7b7f4ae851c3698
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-12 07:50:36 +0300

    Scripted auto-commit on change (2019-10-12 07:50:36) by gitwatch.sh

--- src/main/java/khusnutdinov/bmstu/lab2/FlightTimeArrivalReducer.java
+++ src/main/java/khusnutdinov/bmstu/lab2/FlightTimeArrivalReducer.java
@@ -23,6 +23,7 @@ public class FlightTimeArrivalReducer extends Reducer<SharedKey, Text, Text, Tex
         Double min = Double.MAX_VALUE, max = Double.MIN_VALUE, currentDelay, sum = 0.0;
         int counter = 0;
         String airportName = iterator.next().toString();
+        log.info(airportName);
         while(iterator.hasNext()){
 
             String token = iterator.next().toString();

78c86d90707f7c2a40f7f67377288d804da03734
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-14 21:17:02 +0300

    Scripted auto-commit on change (2019-10-14 21:17:02) by gitwatch.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 echo 
 do
 case "$1" in
--s) echo "******START_HADOOP_BY_KHUSNUTDINOV******\n"
-stop-all.sh && start-all.sh && rm -R output && hadoop fs -rm -R output && echo "\n******STARTING_INITIALISE******\n" && mvn package && hadoop jar target/Khusnutdinov.BMSTU-1.0-SNAPSHOT.jar khusnutdinov.bmstu.lab2.FlightTimeArrival output && hadoop fs -copyToLocal output && cd output && echo "******HERE_IS_THE_OUTPUT******\n" && cat * && cd ..;;
-*)
+-s) echo "******START_ALL_HADOOP_BY_KHUSNUTDINOV******\n" &&
+stop-all.sh && start-all.sh && rm -R ./output && hadoop fs -rm -R output && echo "\n******STARTING_INITIALISE******\n" && mvn package && hadoop jar target/Khusnutdinov.BMSTU-1.0-SNAPSHOT.jar khusnutdinov.bmstu.lab2.FlightTimeArrival output && hadoop fs -copyToLocal output && cd output && echo "******HERE_IS_THE_OUTPUT******\n" && cat * && cd .. ;;
+-d) echo "******START_HADOOP_BY_KHUSNUTDINOV******\n" && rm -R ./output && hadoop fs -rm -R output && echo "\n******STARTING_INITIALISE******\n" && mvn package && hadoop jar target/Khusnutdinov.BMSTU-1.0-SNAPSHOT.jar khusnutdinov.bmstu.lab2.FlightTimeArrival output && hadoop fs -copyToLocal output && cd output && echo "******HERE_IS_THE_OUTPUT******\n" && cat * && cd .. ;;
+*) echo "\n******NO_SUCH_PARAMETER******\n"
+esac
+shift
+done

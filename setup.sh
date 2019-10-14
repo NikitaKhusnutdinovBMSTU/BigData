a94e1634c993d5d4773a329d93451583d3b14120
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-14 21:20:00 +0300

    Scripted auto-commit on change (2019-10-14 21:20:00) by gitwatch.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ while [ -n "$1" ]
 do
 case "$1" in
 -s) echo "******START_ALL_HADOOP_BY_KHUSNUTDINOV******" &&
-stop-all.sh && start-all.sh && rm -R ./output && hadoop fs -rm -R output && echo "******STARTING_INITIALISE******" && mvn package && hadoop jar target/Khusnutdinov.BMSTU-1.0-SNAPSHOT.jar khusnutdinov.bmstu.lab2.FlightTimeArrival output && hadoop fs -copyToLocal output && cd output && echo "******HERE_IS_THE_OUTPUT******" && cat * && cd .. ;;
--d) echo "******START_HADOOP_BY_KHUSNUTDINOV******" && rm -R ./output && hadoop fs -rm -R output && echo "******STARTING_INITIALISE******" && mvn package && hadoop jar target/Khusnutdinov.BMSTU-1.0-SNAPSHOT.jar khusnutdinov.bmstu.lab2.FlightTimeArrival output && hadoop fs -copyToLocal output && cd output && echo "******HERE_IS_THE_OUTPUT******" && cat * && cd .. ;;
+stop-all.sh && start-all.sh && echo "******STARTING_INITIALISE******" && mvn package && hadoop jar target/Khusnutdinov.BMSTU-1.0-SNAPSHOT.jar khusnutdinov.bmstu.lab2.FlightTimeArrival output && hadoop fs -copyToLocal output && cd output && echo "******HERE_IS_THE_OUTPUT******" && cat * && cd .. && rm -R ./output && hadoop fs -rm -R output;;
+-d) echo "******START_HADOOP_BY_KHUSNUTDINOV******" && echo "******STARTING_INITIALISE******" && mvn package && hadoop jar target/Khusnutdinov.BMSTU-1.0-SNAPSHOT.jar khusnutdinov.bmstu.lab2.FlightTimeArrival output && hadoop fs -copyToLocal output && cd output && echo "******HERE_IS_THE_OUTPUT******" && cat * && cd .. && rm -R ./output && hadoop fs -rm -R output ;;
 *) echo "******NO_SUCH_PARAMETER******"
 esac
 shift

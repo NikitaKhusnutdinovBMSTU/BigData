e73b9c1edab08ee3e52e147eb13fd7177b49edb9
Nikita Khusnutdinov <f_ury102@mail.ru>
2019-10-17 00:31:55 +0300

    Scripted auto-commit on change (2019-10-17 00:31:55) by gitwatch.sh

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@ echo
 while [ -n "$1" ]
 do
 case "$1" in
--s) echo "******START_ALL_HADOOP_BY_KHUSNUTDINOV******" &&
+-s) hadoop fs -rm -R output; echo "******START_ALL_HADOOP_BY_KHUSNUTDINOV******" &&
 stop-yarn.sh && stop-dfs.sh && start-dfs.sh && 
 start-yarn.sh && 
 echo "******STARTING_INITIALISE******" && 
 mvn package && hadoop jar target/*.jar khusnutdinov.bmstu.lab2.FlightTimeArrival output && 
 hadoop fs -copyToLocal output && cd output && echo "******HERE_IS_THE_OUTPUT******" && cat * > solution.txt && 
 mv solution.txt ../ && cd .. && rm -R ./output && hadoop fs -rm -R output;;
--d) echo "******START_HADOOP_BY_KHUSNUTDINOV******" && hdfs dfsadmin -safemode leave && echo "******STARTING_INITIALISE******" && mvn package && hadoop jar target/Khusnutdinov.BMSTU-1.0-SNAPSHOT.jar khusnutdinov.bmstu.lab2.FlightTimeArrival output && hadoop fs -copyToLocal output && cd output && echo "******HERE_IS_THE_OUTPUT******" && cat * > solution.txt && 
+-d) hadoop fs -rm -R output; echo "******START_HADOOP_BY_KHUSNUTDINOV******" && hdfs dfsadmin -safemode leave && echo "******STARTING_INITIALISE******" && mvn package && hadoop jar target/Khusnutdinov.BMSTU-1.0-SNAPSHOT.jar khusnutdinov.bmstu.lab2.FlightTimeArrival output && hadoop fs -copyToLocal output && cd output && echo "******HERE_IS_THE_OUTPUT******" && cat * > solution.txt && 
 mv solution.txt ../ && cd .. && rm -R ./output && hadoop fs -rm -R output;;
 *) echo "******USAGE: -d classic_start(without reboot), -s start_with_reboot OUTPUT_IN_SOLUTION.TXT******"
 esac
